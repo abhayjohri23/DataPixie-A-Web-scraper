26f12f886171f6088718921882e5c4e46d3df899
Abhay <abhay.johri0@gmail.com>
2023-06-27 13:37:00 +0530

    Added Connection Pool

---
+++ src/main/java/com/SkillScraper/ConnectionPool.java
@@ -0,0 +1,16 @@
+package com.SkillScraper;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionPool {
+    private static List<Connection> conPool;
+    public static List<Connection> getConPool(){
+        if(conPool.isEmpty()){
+            conPool = new ArrayList<>();
+        }
+
+        return ConnectionPool.conPool;
+    }
+}
